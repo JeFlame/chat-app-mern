83b4d450de2595507da3d1d1827b70f25dbec5d5
Huy Hoàng Vũ <hoangvu130301@gmail.com>
2023-06-09 10:15:20 +0700

    register

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const User = require('./models/User');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+jwtSecret = process.env.JWT_SECRET;
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+
+app.get('/test', (req, res) => {
+    res.json('Hello World!');
+});
+
+// abc
+// abc
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const createdUser = await User.create({ username, password });
+        jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json({
+                _id: createdUser._id,
+            });
+        });
+    } catch (err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
+});
+
+app.listen(4040);
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Register from "./Register";
+import axios from "axios";
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
+  return (
+    <Register />
+  )
+}
+
+export default App
